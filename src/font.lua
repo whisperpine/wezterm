9415770b82fc70a057e67436f11535bcfe74d122
Yusong <yusong.lai@icloud.com>
2024-11-29 21:50:37 +0800

    feat: alter font configurations

--- src/font.lua
+++ src/font.lua
@@ -4,16 +4,14 @@ local wezterm = require "wezterm"
 local module = {}
 
 local font_normal = wezterm.font_with_fallback {
-  { family = "Cascadia Mono",          weight = "Regular" },
-  { family = "Symbols Nerd Font Mono", scale = 0.95 },
-  -- { family = "Cousine Nerd Font" },
-  { family = "Sarasa Mono SC",         weight = "DemiBold" },
+  { family = "Cascadia Mono", weight = "Regular" },
+  { family = "Symbols Nerd Font", scale = 1 },
+  { family = "Sarasa Mono SC", weight = "DemiBold"},
 }
 local font_italic = wezterm.font_with_fallback {
-  { family = "Cascadia Mono",          weight = "Regular",  italic = true },
-  { family = "Symbols Nerd Font Mono", scale = 0.95 },
-  -- { family = "Cousine Nerd Font" },
-  { family = "Sarasa Mono SC",         weight = "DemiBold", italic = true },
+  { family = "Cascadia Mono", weight = "Regular", italic = true },
+  { family = "Symbols Nerd Font", scale = 1 },
+  { family = "Sarasa Mono SC", weight = "DemiBold", italic = true },
 }
 
 -- define a function in the module table.
@@ -23,21 +21,18 @@ local font_italic = wezterm.font_with_fallback {
 -- the config is for them to export an `apply_to_config`
 -- function that accepts the config object, like this:
 function module.apply_to_config(config)
+  config.cell_width = 0.95
+  config.line_height = 0.95
   config.font_size = 11.0
   config.font = font_normal
   config.allow_square_glyphs_to_overflow_width = "Always"
   config.font_rules = {
-    -- For Bold-but-not-italic text, use this relatively bold font, and override
-    -- its color to a tomato-red color to make bold text really stand out.
-    { intensity = "Bold", italic = false, font = font_normal },
-    -- Bold-and-italic
-    { intensity = "Bold", italic = true, font = font_italic },
-    -- normal-intensity-and-italic
+    { intensity = "Normal", italic = false, font = font_normal },
     { intensity = "Normal", italic = true, font = font_italic },
-    -- half-intensity-and-italic (half-bright or dim)
-    { intensity = "Half", italic = true, font = font_italic },
-    -- half-intensity-and-not-italic
+    { intensity = "Bold", italic = false, font = font_normal },
+    { intensity = "Bold", italic = true, font = font_italic },
     { intensity = "Half", italic = false, font = font_normal },
+    { intensity = "Half", italic = true, font = font_italic },
   }
 end
 
